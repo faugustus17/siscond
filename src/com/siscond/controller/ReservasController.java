bde7d05623d2b2a9ec5eb60826ca132310b4941b
Nando <faugustus17@gmail.com>
2017-06-08 21:07:48 -0300

    08/06/2017 - 21:07

---
+++ src/com/siscond/controller/ReservasController.java
@@ -0,0 +1,16 @@
+package com.siscond.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class ReservasController implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
